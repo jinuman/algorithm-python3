400137d33d29df92f6574309f18055d9755cc90d
jinuman <jin35kim@gmail.com>
2018-05-09 02:02:09 +0900

    Modify B10808.py

--- ASCII/B10808.py
+++ ASCII/B10808.py
@@ -3,10 +3,11 @@
 알파벳 소문자로만 이루어진 단어 s
 각 알파벳이 몇개가 포함되어 있는지 구하는 문제
 a의 개수, b의 개수, .... , z의 개수
+
+ASCII Code !!
 chr() : int to char
 ord() : char to int ex) ord("z") = 122
 '''
 s = input()
 for i in range(97, 123):
     print(s.count(chr(i)), end=" ")
-
